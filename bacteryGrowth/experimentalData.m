63a73e24ed27e39c030d3cd6cf555bd87a594abf
pedroperrusi <suruagyperrusi@etu.unistra.fr>
2019-03-20 14:31:40 +0100

    Refactored experiments plot

--- bacteryGrowth/experimentalData.m
+++ bacteryGrowth/experimentalData.m
@@ -2,19 +2,12 @@ clear all; close all;
 %% Load Experimental Data
 load('data/RawData.mat')
 
-[n, numPlot] = size(Calibration.Data);
-
 %% Calibration Courves Plot
-figure; hold on; grid on;
-title('Calibration Courves')
-for i = 1 : numPlot
-   semilogx(Calibration.Time, Calibration.Data(:,i), 'LineWidth', 2)
-end
+plotExpData(Calibration)
 
 %% Measurements Couves Plot
-[n, numPlot] = size(Measurement.Data);
-figure; hold on; grid on;
-title('Measurement Courves')
-for i = 1 : numPlot
-   semilogx(Calibration.Time, Measurement.Data(:,i), 'LineWidth', 2)
-end
+plotExpData(Measurement)
+
+%% Estimate TTD
+low_thresh_level = 0.3; % Optical density threshold level
+indexes = find(Measurement.Data == low_thresh_level);
